37f62b08b029564edd2e1ae6b31989c7863cf505
Musango <musangow@gmail.com>
2022-10-07 13:00:49 +0200

    changes

--- core/src/components/goodbye-world/goodbye-world.tsx
+++ core/src/components/goodbye-world/goodbye-world.tsx
@@ -9,7 +9,7 @@ export class GoodByeWorld {
     return (
       <Host>
         <div>A am a transformer</div>
-        <button>Bring grimworld button</button>
+        <button>Bring grim ki world button</button>
       </Host>
     );
   }
